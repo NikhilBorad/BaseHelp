40072d90964e851b30be5efabd27ef1b41c384a8
Nikhil Borad <nikhilborad8@gmail.com>
2017-12-20 11:30:23 +0530

    kai b

--- app/src/main/java/com/share/basehelplibrary/MainActivity.java
+++ app/src/main/java/com/share/basehelplibrary/MainActivity.java
@@ -5,11 +5,13 @@ import android.os.Bundle;
 
 import com.share.baselibrary.BaseAppCompactActivity;
 
-public class MainActivity extends BaseAppCompactActivity {
+public class MainActivity extends BaseAppCompactActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
